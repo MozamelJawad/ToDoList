af91eee9ed9f68cb322236916a6e2156592629dc
Mozamel Jawad <mozameljawad@gmail.com>
2023-07-27 23:03:30 +0430

    Update remove todos function

--- src/deleteTodo.js
+++ src/deleteTodo.js
@@ -1,14 +1,14 @@
 function removeTodos(item) {
   if (item.classList.contains('editList')) {
-    let stored = JSON.parse(localStorage.getItem('tasks')) || [];
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const list = document.querySelector('#toDoList');
     const index = Number(item.id);
-    stored = stored.filter((object) => object.index !== index);
+    tasks = tasks.filter((object) => object.index !== index);
     list.removeChild(item);
-    stored.forEach((object) => {
+    tasks.forEach((object) => {
       if (object.index > index) { object.index -= 1; }
     });
-    localStorage.setItem('tasks', JSON.stringify(stored));
+    localStorage.setItem('tasks', JSON.stringify(tasks));
     return true;
   }
 
