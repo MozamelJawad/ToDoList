7ac369c94b469b3dc584dd56d3707099106a6e45
Mozamel Jawad <mozameljawad@gmail.com>
2023-07-28 09:50:42 +0430

    update js files

--- src/test/addTodo.test.js
+++ src/test/addTodo.test.js
@@ -1,5 +1,5 @@
-import addTodos from '../addTodo.js'
-import removeTodos from '../deleteTodo.js';
+import addTodos from '../module/addTodo.js';
+import removeTodos from '../module/deleteTodo.js';
 
 // import the `jest-localstorage-mock` library to mock localStorage
 import 'jest-localstorage-mock';
